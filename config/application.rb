6ad927f9acec82de6a6cc26b3922ff9f0455b237
Oyewole Abayomi Samuel <oyewoleabayomi@gmail.com>
2016-05-23 16:06:20 +0100

    modified config/application.rb file

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Wedding
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += [
+    'application.css', 'application.js'
+    ]
+     
   end
 end
