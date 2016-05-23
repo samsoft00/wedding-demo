cd2a40cbb183429a3c40b15c2aa319ca3790aac7
Oyewole Abayomi Samuel <oyewoleabayomi@gmail.com>
2016-05-23 12:44:00 +0100

    modified database and production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Bundler.require(*Rails.groups(assets: %w(development test production)))
 end
