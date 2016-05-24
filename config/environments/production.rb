98f9676b52da6fd33614fad9569d417288d14a2a
Oyewole Abayomi Samuel <oyewoleabayomi@gmail.com>
2016-05-24 09:52:12 +0100

    add params to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = true# ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -39,7 +41,7 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
