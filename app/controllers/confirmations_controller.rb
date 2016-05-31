c330fdca401b7a76d4c3a36a8162d0a2df5db1bb
Oyewole Abayomi Samuel <oyewoleabayomi@gmail.com>
2016-05-31 21:44:04 +0100

    Change auto login on user confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -58,7 +58,8 @@ class ConfirmationsController < Devise::ConfirmationsController
     def do_confirm
       @confirmable.confirm!
       set_flash_message :notice, :confirmed
-      sign_in_and_redirect(resource_name, @confirmable)
+      # sign_in_and_redirect(resource_name, @confirmable)
+      redirect_to new_user_session_path #Login page
     end
     
 end
