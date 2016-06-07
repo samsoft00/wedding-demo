866de2d5df3854438d2ba75215b8df05e39a4673
Oyewole Abayomi Samuel <oyewoleabayomi@gmail.com>
2016-06-07 20:56:00 +0100

    set config on heroku

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,8 +5,8 @@ require "refile/s3"
 aws = {
   access_key_id: ENV["AWS_ACCESS_KEY"],
   secret_access_key: ENV["AWS_SECRET_ACCESS"],
-  region: 'us-east-1',
-  bucket: 'wedding-ng'
+  region: ENV["REGION"],
+  bucket: ENV["BUCKET"]
 }
 
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
